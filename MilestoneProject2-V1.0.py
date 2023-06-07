bde082512676a00fb97326a011f71b7d8dfc4b1f
banosdiego <diegobanos@hotmail.com>
2023-06-07 10:27:16 -0600

    Save MilestoneProject2-V1.0.py.

--- MilestoneProject2-V1.0.py
+++ MilestoneProject2-V1.0.py
@@ -10,8 +10,8 @@
 #CARD CLASS
 #Suit, Rank, Value
 values = {"Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Jack":11, "Queen":12, "King":13, "Ace":14}
-suits = ()
-ranks = ()
+suits = ("Clubs","Diamons","Hearts","Spades")
+ranks = ("Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace")
 class CARD:
     def __init__(self, suit, rank):
 
