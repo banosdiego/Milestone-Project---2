c6cf2bd68203b283cf68c53ee7a489c3583f5171
banosdiego <diegobanos@hotmail.com>
2023-06-23 09:09:34 -0600

    deck class created correctly

--- MilestoneProject2-V1.0.py
+++ MilestoneProject2-V1.0.py
@@ -1,17 +1,11 @@
 #WAR GAME
-
-#52 Cards Deck
-#2 players with 26 Cards Each
-#Card Class
-#Deck Class
-#Player Class
-#Game Logic
-
-#CARD CLASS
-#Suit, Rank, Value
+import random
 values = {"Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Jack":11, "Queen":12, "King":13, "Ace":14}
 suits = ("Clubs","Diamons","Hearts","Spades")
 ranks = ("Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace")
+
+#CARD CLASS
+#Suit, Rank, Value
 class CARD:
     def __init__(self, suit, rank):
 
@@ -22,8 +16,39 @@ class CARD:
     def __str__(self):
         return self.rank + " of " + self.suit
     
+#Deck Class
+#52 Cards Deck
+class DECK:
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                #Create the card object
+                created_card = CARD(suit,rank)
+                self.all_cards.append(created_card)
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.all_cards.pop()
+
+#Player Class
+#2 players with 26 Cards Each
+#Game Logic
 two_hearts = CARD("Hearts", "Two")
 three_clubs = CARD("Clubs","Three")
 a = two_hearts.value < three_clubs.value 
 print(a)
-#Test from ipad, created on ipad to see if its update the whole git project.
+mydeck = DECK()
+b = mydeck.all_cards[-1]
+mydeck.shuffle()
+c = mydeck.all_cards
+for x in c:
+    print(x)
+numero = len(mydeck.all_cards)
+print (numero)
+carta1 = mydeck.deal_one()
+print (carta1)
+numero2 = len(mydeck.all_cards)
+print (numero2)
